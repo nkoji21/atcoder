3a0dfddd1e4b33e79169c2147537b88598862fcc
nkoji21 <133028205+nkoji21@users.noreply.github.com>
2026-01-05 16:28:03 +0900

    templateを更新

--- .config/atcoder-cli-nodejs/go/main.go
+++ .config/atcoder-cli-nodejs/go/main.go
@@ -1,6 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
+
+func sqrt(i int) int {
+	return int(math.Sqrt(float64(i)))
+}
+
+func mod(v, m int) int {
+	if m <= 0 {
+		panic("mod: m must be positive")
+	}
+	r := v % m
+	if r < 0 {
+		r += m
+	}
+	return r
+}
 
 func main() {
 	fmt.Println("Hello World")

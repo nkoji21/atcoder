b374053583829bd019bc2753339dfc1b50d9edc0
nkoji21 <133028205+nkoji21@users.noreply.github.com>
2026-01-11 19:19:31 +0900

    ABS Placing_Marbles

---
+++ _result/ABS/Placing_Marbles/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	N := readString()
+	fmt.Println(N[0] - '0' + N[1] - '0' + N[2] - '0')
+}
+
+// ==============================
+// Input Functions
+// ==============================
+var (
+	reader = bufio.NewReader(os.Stdin)
+)
+
+// int型の入力を読み込む
+func readInt() int {
+	var n int
+	fmt.Fscan(reader, &n)
+	return n
+}
+
+// int型の配列を読み込む
+func readInts(n int) []int {
+	s := make([]int, n)
+	for i := range s {
+		fmt.Fscan(reader, &s[i])
+	}
+	return s
+}
+
+// string型の入力を読み込む
+func readString() string {
+	var s string
+	fmt.Fscan(reader, &s)
+	return s
+}
+
+// string型の配列を読み込む
+func readStrings(n int) []string {
+	s := make([]string, n)
+	for i := range s {
+		fmt.Fscan(reader, &s[i])
+	}
+	return s
+}

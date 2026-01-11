736ac66ea312fb539a32985b1bfcaf55d1b1a8de
nkoji21 <133028205+nkoji21@users.noreply.github.com>
2026-01-11 17:03:42 +0900

    439 a

---
+++ _result/439/a/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+)
+
+func main() {
+	N := readInt()
+	fmt.Println(int(math.Pow(2, float64(N))) - 2*N)
+}
+
+// ==============================
+// Input Functions
+// ==============================
+var (
+	reader = bufio.NewReader(os.Stdin)
+)
+
+// int型の入力を読み込む
+func readInt() int {
+	var n int
+	fmt.Fscan(reader, &n)
+	return n
+}
+
+// int型の配列を読み込む
+func readInts(n int) []int {
+	s := make([]int, n)
+	for i := range s {
+		fmt.Fscan(reader, &s[i])
+	}
+	return s
+}
+
+// string型の入力を読み込む
+func readString() string {
+	var s string
+	fmt.Fscan(reader, &s)
+	return s
+}
+
+// string型の配列を読み込む
+func readStrings(n int) []string {
+	s := make([]string, n)
+	for i := range s {
+		fmt.Fscan(reader, &s[i])
+	}
+	return s
+}

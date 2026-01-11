1dba1f170e6abe86a9da8e5c74511fdc3bc9aa0c
nkoji21 <133028205+nkoji21@users.noreply.github.com>
2026-01-11 19:07:47 +0900

    ABS practiceA

---
+++ _result/ABS/practiceA/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	var a, b, c int
+	var s string
+	fmt.Scan(&a, &b, &c, &s)
+
+	fmt.Println(a+b+c, s)
+}
+
+// ==============================
+// Input Functions
+// ==============================
+var (
+	reader = bufio.NewReader(os.Stdin)
+)
+
+// int型の入力を読み込む
+func readInt() int {
+	var n int
+	fmt.Fscan(reader, &n)
+	return n
+}
+
+// int型の配列を読み込む
+func readInts(n int) []int {
+	s := make([]int, n)
+	for i := range s {
+		fmt.Fscan(reader, &s[i])
+	}
+	return s
+}
+
+// string型の入力を読み込む
+func readString() string {
+	var s string
+	fmt.Fscan(reader, &s)
+	return s
+}
+
+// string型の配列を読み込む
+func readStrings(n int) []string {
+	s := make([]string, n)
+	for i := range s {
+		fmt.Fscan(reader, &s[i])
+	}
+	return s
+}

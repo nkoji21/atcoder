e64ed4e402354af0f8ca1ec3d506998c49a9d195
nkoji21 <133028205+nkoji21@users.noreply.github.com>
2026-01-13 09:20:34 +0900

    _abs Kagami_Mochi

---
+++ _result/_abs/Kagami_Mochi/main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"slices"
+	"sort"
+)
+
+func main() {
+	N := readInt()
+	D := readInts(N)
+
+	sort.Ints(D)
+	unique := slices.Compact(D)
+	fmt.Println(len(unique))
+}
+
+// ==============================
+// Input Functions
+// ==============================
+var (
+	reader = bufio.NewReader(os.Stdin)
+)
+
+// int型の入力を読み込む
+func readInt() int {
+	var n int
+	fmt.Fscan(reader, &n)
+	return n
+}
+
+// int型の配列を読み込む
+func readInts(n int) []int {
+	s := make([]int, n)
+	for i := range s {
+		fmt.Fscan(reader, &s[i])
+	}
+	return s
+}
+
+// string型の入力を読み込む
+func readString() string {
+	var s string
+	fmt.Fscan(reader, &s)
+	return s
+}
+
+// string型の配列を読み込む
+func readStrings(n int) []string {
+	s := make([]string, n)
+	for i := range s {
+		fmt.Fscan(reader, &s[i])
+	}
+	return s
+}

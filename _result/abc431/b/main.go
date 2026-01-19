3ccf1cb5e124eff5a955f3473211500260def2d5
nkoji21 <133028205+nkoji21@users.noreply.github.com>
2026-01-19 18:29:05 +0900

    abc431 b

--- _result/abc431/b/main.go
+++ _result/abc431/b/main.go
@@ -1,7 +1,67 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	X, N := readInt(), readInt()
+	W := readInts(N)
+
+	hasW := make([]bool, N)
+
+	Q := readInt()
+	for i := 0; i < Q; i++ {
+		P := readInt() - 1
+
+		if hasW[P] {
+			X -= W[P]
+		} else {
+			X += W[P]
+		}
+
+		hasW[P] = !hasW[P]
+		fmt.Println(X)
+	}
+}
+
+// ==============================
+// Input Functions
+// ==============================
+var (
+	reader = bufio.NewReader(os.Stdin)
+)
+
+// int型の入力を読み込む
+func readInt() int {
+	var n int
+	fmt.Fscan(reader, &n)
+	return n
+}
+
+// int型の配列を読み込む
+func readInts(n int) []int {
+	s := make([]int, n)
+	for i := range s {
+		fmt.Fscan(reader, &s[i])
+	}
+	return s
+}
+
+// string型の入力を読み込む
+func readString() string {
+	var s string
+	fmt.Fscan(reader, &s)
+	return s
+}
+
+// string型の配列を読み込む
+func readStrings(n int) []string {
+	s := make([]string, n)
+	for i := range s {
+		fmt.Fscan(reader, &s[i])
+	}
+	return s
 }

a02eac5f10f8f08083387379e414ea60bb6a8a4d
nkoji21 <133028205+nkoji21@users.noreply.github.com>
2026-04-22 00:08:05 +0900

    abc454 d refc

--- _result/abc454/d/main.cpp
+++ _result/abc454/d/main.cpp
@@ -5,15 +5,13 @@ using namespace std;
 左から読みながら「末尾が (xx) になったら即 xx にする」を繰り返すスタック実装
 */
 
-static string reduce(string s) {
+static string reduce(const string& s) {
   string t;
   for (char c : s) {
     t.push_back(c);
-    while (t.size() >= 4 && t[t.size() - 4] == '(' && t[t.size() - 3] == 'x' &&
-           t[t.size() - 2] == 'x' && t[t.size() - 1] == ')') {
+    while (t.size() >= 4 && t.substr(t.size() - 4) == "(xx)") {
       t.resize(t.size() - 4);
-      t.push_back('x');
-      t.push_back('x');
+      t += "xx";
     }
   }
 
